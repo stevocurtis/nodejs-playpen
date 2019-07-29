3f397281962a22dba7437a22b8540f12d7422990
stevocurtis <myste123>
2019-07-30 00:38:34 +0100

    Use itilUtil.itilPrepend for spec comparison

--- src/integration/inlife/itil-util.spec.js
+++ src/integration/inlife/itil-util.spec.js
@@ -9,27 +9,27 @@ describe("Itil Utility Tests", function() {
     chai.assert.isUndefined(itilUtil.convertTmfDescriptionToItilClass(""));
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("something"),
-      "itil_class_id_something"
+      itilUtil.itilPrepend + "something"
     );
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("Something"),
-      "itil_class_id_something"
+      itilUtil.itilPrepend + "something"
     );
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("SOMETHING"),
-      "itil_class_id_something"
+      itilUtil.itilPrepend + "something"
     );
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("Something With Spaces"),
-      "itil_class_id_something_with_spaces"
+      itilUtil.itilPrepend + "something_with_spaces"
     );
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("SomeThinG WiTh sPacEs"),
-      "itil_class_id_something_with_spaces"
+      itilUtil.itilPrepend + "something_with_spaces"
     );
     chai.assert.equal(
       itilUtil.convertTmfDescriptionToItilClass("SOMETHING WITH SPACES"),
-      "itil_class_id_something_with_spaces"
+      itilUtil.itilPrepend + "something_with_spaces"
     );
   });
 });
