cc2ed024472b1bdde67832156f0b01d374eaae03
Steve Curtis <steven.curtis@bt.com>
2019-07-29 13:39:00 +0100

    Added first cut of parsing bss inventory broadcast

--- src/data/bss/bss-inventory-broadcast.js
+++ src/data/bss/bss-inventory-broadcast.js
@@ -1,6 +1,60 @@
 module.exports = {
-    processOnmi: function(payload) {
-        console.log('processOmni received', payload);
-        return true;
+    getAssetProcessItems: function(payload) {
+        let processedOrderItems = [];
+        // console.log('getAsseprocessItems received raw', payload);
+        // console.log('getAsseprocessItems received stringified', JSON.stringify(payload));
+
+        // console.log(payload.Order);
+        if (payload && payload.Order && payload.Order.orderItem) {
+            payload.Order.orderItem.forEach(orderItem => {
+                // console.log('found an order item', orderItem);
+                console.log('~~~~~~~~~~~~~~~~~~~~~~~~~');
+                processedOrderItems.push({
+                    'orderItem': orderItem,
+                    'id': getAncestordIdFromOrderItem(orderItem, 0),
+                    'parentId': getAncestordIdFromOrderItem(orderItem, 1),
+                    'type': getAssedTypeFromOrderItem(orderItem)
+                })
+            });
+        } else {
+            console.error('bss inventory does not contain and items');
+        }
+        console.log('processedOrderItems', processedOrderItems);
+        return processedOrderItems;
     }
+}
+
+function getAssedTypeFromOrderItem(orderItem) {
+    let type = undefined;
+    try {
+        type = orderItem.entity.entitySpecKey.type;
+    } catch (error) {
+        console.error('error parsing type', error);
+    }
+    console.log('returning type', type);
+    return type;
+}
+
+function getAssedIdXpathFromOrderItem(orderItem) {
+    primaryKeyXpath = orderItem.entity.entitySpecKey.primaryKey.value;
+    console.log('found order item primary xpath key', primaryKeyXpath);
+    xpathTokens = primaryKeyXpath.split('/');
+    console.log('found order item xpathTokens', xpathTokens);
+}
+
+function getAncestordIdFromOrderItem(orderItem, ancestorLevel) {
+    let id = undefined;
+    try {
+        primaryKeyXpath = orderItem.entity.entitySpecKey.primaryKey.value;
+        console.log('found order item primary xpath key', primaryKeyXpath);
+        xpathTokens = primaryKeyXpath.split('/');
+        // if (xpathTokens && xpathTokens.lo)
+        console.log('found order item xpathTokens', xpathTokens);
+        console.log('found', xpathTokens.length, 'order item xpathTokens');
+        id = xpathTokens[xpathTokens.length - (ancestorLevel + 1)];
+    } catch (error) {
+        console.error('error parsing id', error);
+    }
+    console.log('returning ancestor id', id);
+    return id;
 }
--- src/data/bss/bss-inventory-broadcast.spec.js
+++ src/data/bss/bss-inventory-broadcast.spec.js
@@ -1,9 +1,21 @@
 var bssInventoryBroadcast = require('./bss-inventory-broadcast');
 var chai = require('chai');
+const fs = require('fs');
 
 describe('BSS Inventory Broadcast Tests', function() {
 
-    it('should be truthy', function() {
-        chai.assert.isOk(bssInventoryBroadcast.processOnmi('test'));
+    it('should parse order items', function() {
+
+        let bssExampleInventoryBroadcastRaw = fs.readFileSync('src/data/bss/bss-inventory-broadcast-sample.json');
+        let bssExampleInventoryBroadcast = JSON.parse(bssExampleInventoryBroadcastRaw);
+
+        let processedOrderItems = bssInventoryBroadcast.getAssetProcessItems(bssExampleInventoryBroadcast);
+
+        // check number of items
+        chai.assert.isOk(processedOrderItems.length, 15, 'different number of order items processed');
+        // check id and parent id
+        chai.assert.isOk(processedOrderItems[0].id, '5a625fe3-9db4-40cd-82c0-169c45d68569', 'primary id does not match');
+        chai.assert.isOk(processedOrderItems[0].parentId, 'de39707b-d775-46a5-b61a-e501fac0e6f9', 'parent id does not match');
+        chai.assert.isOk(processedOrderItems[0].type, 'CFS', 'type does not match');
     });
 });
