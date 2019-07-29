6f8c7b67239338f6a9ebe1659b4e4a25fd0f8608
stevocurtis <myste123>
2019-07-30 00:39:17 +0100

    Tidied up some whitespace formatting

--- src/integration/inlife/itil-util.js
+++ src/integration/inlife/itil-util.js
@@ -2,7 +2,6 @@ module.exports = {
   //
   // This function converts a TML description to an ITIL class
   //
-
   convertTmfDescriptionToItilClass: function(tmfDescription) {
     let itilClass = undefined;
     if (tmfDescription && tmfDescription.length > 0) {
