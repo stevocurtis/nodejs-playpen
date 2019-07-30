c0757ddd04df74e509a93e2ebbbedb22e9e893b9
Steve Curtis <steven.curtis@bt.com>
2019-07-30 11:49:40 +0100

    Made note on the fact that inlife asset response is a best guess

--- src/integration/inlife/inlife-inventory-write.js
+++ src/integration/inlife/inlife-inventory-write.js
@@ -11,6 +11,7 @@ module.exports = {
     //
     writeInlifeAsset: function(enhancedBssItem) {
         // do not care about the stub response as long as it has a unique id
+        // TODO correct this format
         let response = {
             sys_id: uuid4()
         };
