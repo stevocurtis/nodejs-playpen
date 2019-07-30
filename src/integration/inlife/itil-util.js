049df43ba5771123136e7b58ce9f727544a17043
Steve Curtis <steven.curtis@bt.com>
2019-07-30 09:45:55 +0100

    Corrected itil class constant

--- src/integration/inlife/itil-util.js
+++ src/integration/inlife/itil-util.js
@@ -1,14 +1,14 @@
 module.exports = {
-  //
-  // This function converts a TML description to an ITIL class
-  //
-  convertTmfDescriptionToItilClass: function(tmfDescription) {
-    let itilClass = undefined;
-    if (tmfDescription && tmfDescription.length > 0) {
-      itilClass =
-        this.itilPrepend + tmfDescription.replace(/\ /g, "_").toLowerCase();
-    }
-    return itilClass;
-  },
-  itilPrepend: "itil_class_id_"
+    //
+    // This function converts a TML description to an ITIL class
+    //
+    convertTmfDescriptionToItilClass: function(tmfDescription) {
+        let itilClass = undefined;
+        if (tmfDescription && tmfDescription.length > 0) {
+            itilClass =
+                this.itilPrepend + tmfDescription.replace(/\ /g, "_").toLowerCase();
+        }
+        return itilClass;
+    },
+    itilPrepend: "cmdb_ci_u_"
 };
