7267cddcd2539eb93aef207d1d5490ea1a7b384b
Steve Curtis <steven.curtis@bt.com>
2019-07-30 11:49:05 +0100

    Added stub for inlife asset response

--- src/integration/inlife/inlife-inventory-write.js
+++ src/integration/inlife/inlife-inventory-write.js
@@ -1,13 +1,21 @@
+const uuid4 = require('uuid/v4');
+
 module.exports = {
     //
-    // This function writes multiple inventory into inlfe system
+    // This function writes multiple inventory into inlfe system - stub to simulate a unique id back from inlife system
     //
     writeInlifeAssets: function(enhancedBssItems) {},
 
     //
     // This function writes single inventory into inlfe system
     //
-    writeInlifeAsset: function(enhancedBssItem) {},
+    writeInlifeAsset: function(enhancedBssItem) {
+        // do not care about the stub response as long as it has a unique id
+        let response = {
+            sys_id: uuid4()
+        };
+        return response;
+    },
 
     //
     // This function writes single inventory relationship into inlfe system
--- src/integration/inlife/inlife-inventory-write.spec.js
+++ src/integration/inlife/inlife-inventory-write.spec.js
@@ -3,7 +3,9 @@ var chai = require('chai');
 
 describe('Inlife Inventory Tests', function() {
 
-    it('should be truthy', function() {
-        chai.assert.isOk(true);
+    it('asset write should generate a response with random sys_id', function() {
+        let assetWriteResponse = inlifeInventoryWrite.writeInlifeAsset();
+        chai.assert.isOk(assetWriteResponse.sys_id);
+        chai.assert.isTrue(assetWriteResponse.sys_id.length > 0);
     });
 });
