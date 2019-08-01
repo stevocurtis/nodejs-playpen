4fd8e037163e3a56213dfd7a6ae300625df94654
Steve Curtis <steven.curtis@bt.com>
2019-08-01 11:21:20 +0100

    Updated unit test

--- src/integration/oss/oss-inventory-broadcast.spec.js
+++ src/integration/oss/oss-inventory-broadcast.spec.js
@@ -3,47 +3,47 @@ var chai = require("chai");
 const fs = require("fs");
 
 describe("OSS Inventory Broadcast Tests", function() {
-  beforeEach(function() {
-    let ossExampleInventoryBroadcastRaw = fs.readFileSync(
-      "src/integration/oss/oss-inventory-broadcast-sample.json"
-    );
-    let ossExampleInventoryBroadcast = JSON.parse(
-      ossExampleInventoryBroadcastRaw
-    );
+    beforeEach(function() {
+        let ossExampleInventoryBroadcastRaw = fs.readFileSync(
+            "src/integration/oss/oss-inventory-broadcast-sample.json"
+        );
+        let ossExampleInventoryBroadcast = JSON.parse(
+            ossExampleInventoryBroadcastRaw
+        );
 
-    this.enrichedOrderItems = ossInventoryBroadcast.parseAndEnrichOSSInventoryBroadcast(
-      ossExampleInventoryBroadcast
-    );
-  });
+        this.enrichedOrderItems = ossInventoryBroadcast.parseAndEnrichOSSInventoryBroadcast(
+            ossExampleInventoryBroadcast
+        );
+    });
 
-  it("should parse order items", function() {
-    // check number of items
-    chai.assert.equal(
-      this.enrichedOrderItems.length,
-      1,
-      "different number of order items processed"
-    );
-    // check id and parent id
-    chai.assert.equal(
-      this.enrichedOrderItems[0].id,
-      "SC.BT-SDW-POC2RFS_VNF.RFS_VNF_CITIBANK_RIVERSIDE.24B7208C-7F000001-71ACC80D-A686EB4F",
-      "primary id does not match"
-    );
-    // chai.assert.equal(
-    //   this.enrichedOrderItems[0].parentId,
-    //   "de39707b-d775-46a5-b61a-e501fac0e6f9",
-    //   "parent id does not match"
-    // );
-    chai.assert.equal(
-      this.enrichedOrderItems[0].type,
-      "RFS",
-      "type does not match"
-    );
-  });
+    it("should parse order items", function() {
+        // check number of items
+        chai.assert.equal(
+            this.enrichedOrderItems.length,
+            1,
+            "different number of order items processed"
+        );
+        // check id and parent id
+        chai.assert.equal(
+            this.enrichedOrderItems[0].id,
+            "SC.BT-SDW-POC2RFS_CPE.RFS_CPE_CITIBANK_RIVERSIDE.4C861D1A-7F000001-71ACC80D-381E2604",
+            "primary id does not match"
+        );
+        // chai.assert.equal(
+        //   this.enrichedOrderItems[0].parentId,
+        //   "de39707b-d775-46a5-b61a-e501fac0e6f9",
+        //   "parent id does not match"
+        // );
+        chai.assert.equal(
+            this.enrichedOrderItems[0].type,
+            "RFS",
+            "type does not match"
+        );
+    });
 
-  it("should process enriched order items", function() {
-    // process items
-    // TODO work here this evening
-    // ossInventoryBroadcast.writeEnrichedOSSInventoryItems(this.enrichedOrderItems);
-  });
+    it("should process enriched order items", function() {
+        // process items
+        // TODO work here this evening
+        // ossInventoryBroadcast.writeEnrichedOSSInventoryItems(this.enrichedOrderItems);
+    });
 });
