cf693295ab999ba846d0f35b57df326586a55605
Steve Curtis <steven.curtis@bt.com>
2019-08-01 16:17:38 +0100

    Turn on test for all spec files

--- src/integration/inlife/itil-util.js
+++ src/integration/inlife/itil-util.js
@@ -1,16 +1,16 @@
 module.exports = {
-  //
-  // This function converts a TML description to an ITIL class
-  //
-  convertTmfDescriptionToItilClass: function(tmfDescription) {
-    let itilClass = undefined;
-    if (tmfDescription && tmfDescription.length > 0) {
-      itilClass =
-        this.itilPrepend + tmfDescription.replace(/\ /g, "_").toLowerCase();
-    }
-    return itilClass;
-  },
-  itilPrepend: "u_cmdb_ci_",
-  ossRFSClass: this.itilPrepend + "rfs",
-  ossResourceClass: this.itilPrepend + "resource"
+    //
+    // This function converts a TML description to an ITIL class
+    //
+    convertTmfDescriptionToItilClass: function(tmfDescription) {
+        let itilClass = undefined;
+        if (tmfDescription && tmfDescription.length > 0) {
+            itilClass =
+                this.itilPrepend + tmfDescription.replace(/\ /g, "_").toLowerCase();
+        }
+        return itilClass;
+    },
+    itilPrepend: "u_cmdb_ci_",
+    ossRFSClass: "u_cmdb_ci_rfs",
+    ossResourceClass: "u_cmdb_ci_resource"
 };
